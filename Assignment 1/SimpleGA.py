294e3f90400669dd158d82f83c87e719b1167241
Niek <derksen394@gmail.com>
2021-02-02 00:14:50 +0100

    Added fitness of final found solution for 4b as well as executed 4c.
    Added graph is performance under circumstances of 4c.

--- Assignment 1/SimpleGA.py
+++ Assignment 1/SimpleGA.py
@@ -10,15 +10,20 @@ def SimpleGA(l=100, iterations=1500):
         # b) create a copy of x and invert each of its bits with probability p. Let x_m be the result.
         x_m = [int(not bit) if np.random.rand() < p else bit for bit in x]
         # c) if x_m is closer to the goal sequence than x then replace x with x_m.
-        if np.sum(x_m) > np.sum(x):
-            x = x_m
+        # if np.sum(x_m) > np.sum(x):
+        x = x_m
         memory.append(np.sum(x))
         iterations -= 1
 
+    print("number of ones in found solution: {}".format(np.sum(x)))
+
+    fig = plt.figure()
     plt.xlabel("Number of Iterations")
     plt.ylabel("Number of Ones")
     plt.plot(range(0, len(memory)), memory)
+    fig.savefig("TheCountingOnes_SimpleGA_{}it_{}bits.jpg".format(len(memory), l))
     plt.show()
+
 
 def main():
     SimpleGA()
